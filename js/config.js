154756b99719b9263eef97bb706a8cbd3110a6ff
Bernabe Del Moral <bernabedelmoral@M4-berna.local>
2026-04-17 14:52:56 -0600

    Fix: use JWT anon key

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@
 // ============================================================
 
 const SUPABASE_URL = 'https://baapcdepyieanqdcrlbo.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_d86hQPtiYJp6yZ5ifmo9VA_9ObYLqOn';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJhYXBjZGVweWllYW5xZGNybGJvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY0NDUyMTMsImV4cCI6MjA5MjAyMTIxM30.OxI7DYPBT7UyGNk350b0rHd7YdGKzhNuKO7iH57Ui6I';
 
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
